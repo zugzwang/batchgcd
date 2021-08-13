58259b71e9120804fa50f421367af3fce12e2133
Francisco Vial-Prado <francisco.vialprado@fortanix.com>
2021-08-13 14:12:36 +0200

    Update download link

--- scripts/patch_gmp.sh
+++ scripts/patch_gmp.sh
@@ -11,30 +11,29 @@ cd gmp
 get="fetch";
 ! __exists fetch && get="curl -OL";
 
-if [ ! -f gmp-6.1.2.tar.bz2 ];
+if [ ! -f gmp-6.2.1.tar.xz ];
 then
-    $get ftp://ftp.gmplib.org/pub/gmp-6.1.2/gmp-6.1.2.tar.bz2
+    $get https://gmplib.org/download/gmp/gmp-6.2.1.tar.xz
 fi
 
 
-sum=`openssl sha1 gmp-6.1.2.tar.bz2 | awk -F' ' '{print $2}'`
+sum=`openssl sha1 gmp-6.2.1.tar.xz | awk -F' ' '{print $2}'`
 
 if [[ $sum != "366ded6a44cd108ba6b3f5b9a252eab3f3a95cdf" ]];
 then
     echo ''
     echo '=========================================='
-    echo 'ERROR: could not verify gmp-6.1.2.tar.bz2;'
-    echo 'Downloaded over untrusted channel (non-https)'
+    echo 'ERROR: could not verify gmp-6.2.1.tar.xz;'
     echo '=========================================='
     exit;
 fi
 
-echo 'gmp-6.1.2.tar.bz2 downloaded and verified sha1 signature'
+echo 'gmp-6.2.1.tar.xz downloaded and verified sha1 signature'
 
-tar xf gmp-6.1.2.tar.bz2
+tar xf gmp-6.2.1.tar.xz
 
-cd gmp-6.1.2
-patch -p 1 < ../gmp-6.1.2.patch
+cd gmp-6.2.1
+patch -p 1 < ../gmp-6.2.1.patch
 mkdir ../patched
 ./configure --prefix=$PWD/../patched/
 make
